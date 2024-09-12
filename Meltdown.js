0d2613b5327e782b08fcfccc848af7676715fb8c
Phusara <pozzuoli97@gmail.com>
2024-09-12 11:33:46 +0700

    update Stress

--- Meltdown.js
+++ Meltdown.js
@@ -1,7 +1,6 @@
-/*Write a function removeDuplicates(arr) that removes all duplicate elements from an array and returns the unique elements.
+/*
 Create a function capitalizeWords(arr) that takes an array of strings and returns a new array where each string is capitalized (first letter uppercase).
 Create a function filterPrimes(arr) that takes an array of numbers and returns a new array with only the prime numbers.
-Write a function removeFalsyValues(arr) that removes all falsy values (like 0, null, undefined, false, "", NaN) from an array.
 Write a function countPositives(arr) that counts how many positive numbers exist in an array.
 Write a function isSorted(arr) that checks whether an array is sorted in ascending order. Return true if it is, otherwise false.
 Write a function createRange(start, end) that returns an array with numbers starting from start to end
@@ -13,7 +12,21 @@ function removeDuplicates(arr){
 }
 console.log(removeDuplicates(["Hi","Hi","Hey","Hello"]))
 
-function filterPrimes(arr){
-   
+function removeFalsyValues(arr){
+    let result = arr.filter(falsy => Boolean(falsy));
+    return result
 }
-console.log(filterPrimes([1,2,5,13,50]))
+console.log(removeFalsyValues([null,5,8,0,NaN,""]))
+
+
+function countPositives(arr){
+let count = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] >= 0){
+            count++
+        }
+    }
+    return count
+}
+console.log(countPositives([1,5,7,9,-5000,-8,10]))
+
